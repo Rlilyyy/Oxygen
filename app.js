739ebef19cc7c2675286e8f2b5e4547b79eca29a
bingohlee <bingohlee@gmail.com>
2016-03-20 20:46:52 +0800

    修复IE 9下的bug & 测试后台渲染

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express)
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +24,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+
+app.get("/index", function(req, res) {
+    res.render("index");
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +62,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen("80");
 
 module.exports = app;
